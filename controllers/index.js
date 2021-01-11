18824074cd5e637f427401a23aba0db6c9a5e17b
Md. Farhan Sadiq <mdfarhansadiq01@gmail.com>
2021-01-11 14:00:10 +0600

    Code

---
+++ controllers/index.js
@@ -0,0 +1,21 @@
+
+var express = require('express'),
+    router = express.Router();
+
+router.use('/users', require('./users'));
+router.use('/flashes', require('./flashes'));
+
+router.get('/', function (req, res) {
+    res.render('index', {title: 'Boilerplate'});
+});
+
+router.get('*', function (req, res) {
+    res.status(404).render('error', {
+        title: 'Boilerplate', error: {
+            status: 404,
+            stack: 'Not found'
+        }
+    });
+});
+
+module.exports = router;
